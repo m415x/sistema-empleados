926f24cafb0d8f48b79ca14d8e0162529379ec5b
Cristian Lahoz <lahozcristian@gmail.com>
2022-11-28 22:44:39 -0300

    Conexión a MySQL y test

--- src/app.py
+++ src/app.py
@@ -4,6 +4,25 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    sql = "INSERT INTO empleados (nombre, correo, foto) VALUES ('Juan', 'juan@email.com', 'fotodejuan.jpg');"
+    cursor.execute(sql)
+    
+    conn.commit()
+    
+    return render_template('empleados/index.html')
+
 
 
 if __name__ == '__main__':
